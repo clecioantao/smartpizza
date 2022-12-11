8050f87d8caea8d7586e114fedf331a6eee7f706
Mac Clecio <cleciodev.mac@gmail.com>
2022-12-11 19:16:12 -0300

    Projeto finalizado

--- frontend/src/pages/dashboard/index_bk1.tsx
+++ frontend/src/pages/dashboard/index_bk1.tsx
@@ -72,27 +72,6 @@ export default function Dashboard({ orders }: HomeProps){
   }
 
 
-  async function handleFinishItem(id: string){
-    const apiClient = setupAPIClient();
-    await apiClient.put('/order/finish', {
-      order_id: id,
-    })
-
-    const response = await apiClient.get('/order/list');
-
-    setOrderList(response.data);
-    setModalVisible(false);
-  }
-
-
-  async function handleRefreshOrders(){
-    const apiClient = setupAPIClient();
-
-    const response = await apiClient.get('/order/list')
-    setOrderList(response.data);
-
-  }
-
   Modal.setAppElement('#__next');
 
   return(
@@ -107,18 +86,12 @@ export default function Dashboard({ orders }: HomeProps){
 
         <div className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button onClick={handleRefreshOrders}>
+          <button>
             <FiRefreshCcw size={25} color="#3fffa3"/>
           </button>
         </div>
 
         <article className={styles.listOreders}>
-
-          {orderList.length === 0 && (
-            <span className={styles.emptyList}>
-              Nenhum pedido aberto foi encontrado...
-            </span>
-          )}
 
           {orderList.map( item => (
             <section  key={item.id} className={styles.orderItem}> 
@@ -138,7 +111,6 @@ export default function Dashboard({ orders }: HomeProps){
           isOpen={modalVisible}
           onRequestClose={handleCloseModal}
           order={modalItem}
-          handleFinishOrder={ handleFinishItem }
         />
       )}
 
