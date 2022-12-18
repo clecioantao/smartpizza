dd91ed3007feb0233f782cbc7046009189003085
Mac Clecio <cleciodev.mac@gmail.com>
2022-12-18 19:03:08 -0300

    Projeto finalizado

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,8 +1,37 @@
 import React, { useState }  from 'react';
 import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet} from 'react-native'
 
+import { useNavigation } from '@react-navigation/native'
+
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { StackPramsList } from '../../routes/app.routes'
+
+import { api } from '../../services/api'
 
 export default function Dashboard(){
+  const navigation = useNavigation<NativeStackNavigationProp<StackPramsList>>();
+
+  const [number, setNumber] = useState('');
+ 
+
+  async function openOrder(){
+    if(number === ''){
+      return;
+    }
+
+    const response = await api.post('/order', {
+      table: Number(number)
+    })
+
+    //console.log(response.data);
+
+    navigation.navigate('Order', { number: number, order_id: response.data.id })
+
+    setNumber('');
+
+  }
+  
+
   return(
     <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Novo pedido</Text>
@@ -12,9 +41,11 @@ export default function Dashboard(){
           placeholderTextColor="#F0F0F0"
           style={styles.input}
           keyboardType="numeric"
+          value={number}
+          onChangeText={setNumber}
         />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={openOrder}>
           <Text style={styles.buttonText}>Abrir mesa</Text>
         </TouchableOpacity>
 
